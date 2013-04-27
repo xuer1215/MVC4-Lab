e85434839ffe7d31d3a172a0cd3b60e8635144b9
xuehan <xhdev12@gmail.com>
2013-04-27 23:09:42 +1000

    create RSVP form

---
+++ PartyInvites-Git/PartyInvites-Git/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites_Git.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Greeting = GreetingMessage();
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        private static string GreetingMessage()
+        {
+            return DateTime.Now.Hour < 12 ? "Good morning" : "Good afternoon";
+        }
+
+    }
+}
